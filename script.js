0c3521ea14595f5d251a8226ea59a5c82f79cd36
UGUR PEKUNSAL <32599420+ugurpekunsal@users.noreply.github.com>
2021-03-09 13:19:17 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -6,13 +6,9 @@ const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
 const btnScrollTo = document.querySelector(".btn--scroll-to");
 const section1 = document.querySelector("#section--1");
-const tabs = document.querySelectorAll(".operations__tab");
-const tabsContainer = document.querySelector(".operations__tab-container");
-const tabsContent = document.querySelectorAll(".operations__content");
 
 ///////////////////////////////////////
 // Modal window
-
 const openModal = function (e) {
   e.preventDefault();
   modal.classList.remove("hidden");
@@ -36,25 +32,21 @@ document.addEventListener("keydown", function (e) {
 });
 
 ///////////////////////////////////////
-// Tabbed component
+// Button scrolling
+btnScrollTo.addEventListener("click", function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
 
-tabsContainer.addEventListener("click", function (e) {
-  const clicked = e.target.closest(".operations__tab");
+  console.log(e.target.getBoundingClientRect());
 
-  // Guard clause
-  if (!clicked) return;
+  console.log("Current scroll (X/Y)", window.pageXOffset, window.pageYOffset);
 
-  // Remove active classes
-  tabs.forEach((t) => t.classList.remove("operations__tab--active"));
-  tabsContent.forEach((c) => c.classList.remove("operations__content--active"));
-
-  // Activate tab
-  clicked.classList.add("operations__tab--active");
-
-  // Activate content area
-  document
-    .querySelector(`.operations__content--${clicked.dataset.tab}`)
-    .classList.add("operations__content--active");
+  console.log(
+    "height/width viewport",
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+  section1.scrollIntoView({ behavior: "smooth" });
 });
 
 ///////////////////////////////////////
